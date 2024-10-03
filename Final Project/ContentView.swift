94c929122522bfd4e8974ad01a7f4ef5dec0776a
tyler0611 <berlity0611@gmail.com>
2024-10-03 11:37:44 -0500

    Initial Commit

---
+++ Final Project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Final Project
+//
+//  Created by Tyler Berlin on 10/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
